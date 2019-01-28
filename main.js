7ae096fd868feaef02a4f0a2d5d0395e35030f5f
ThomasDevv <38764266+ThomasDevv@users.noreply.github.com>
2019-01-28 16:51:31 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+const sqDigits = (int) => {
+  if(isNaN(int)) return console.log('You need to pass a number.')
+
+  let arr = int.toString().split('')
+  let index = 0
+  let result = "0"
+
+  for(const element of arr) {
+    result += Math.pow(element, 2)
+  }
+
+  return console.log(parseInt(result.slice(1)))
+}
+
+// Example of usage: sqDigits(23)
+// Returns: 49
